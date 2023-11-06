d37d666ab15194492e8ab854af00ab7c529f1bd1
Basrah123 <elijah_weeks@stu.indianhills.edu>
2023-11-06 11:07:56 -0600

    adding display attribute

--- Models/Course.cs
+++ Models/Course.cs
@@ -4,14 +4,23 @@ namespace CourseCatalog.Models
 {
     public class Course
     {
+        
         public int Id { get; set; }
+        
+        [Display(Name = "Course Name")]
         public string? CourseName { get; set; }
+        
+        [Display(Name = "Course Description")]
         public string? CourseDescription { get; set; }
+        
+        [Display(Name = "Room Number")]
         public int RoomNumber { get; set; }
 
+        [Display(Name = "Start Time")]
         [DataType(DataType.Time)]
         public TimeOnly StartTime { get; set; }
         
+        [Display(Name = "End Time")]
         [DataType(DataType.Time)]
         public TimeOnly EndTime { get; set; }
     }
